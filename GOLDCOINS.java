926ab722d7beccee9d6bada88ae8121965baab14
Hasini E <hasini1530@gmail.com>
2024-12-10 22:35:13 +0530

    GOLDCOINS.java

---
+++ GOLDCOINS.java
@@ -0,0 +1,18 @@
+//START147
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args)
+	{
+	 Scanner sc=new Scanner(System.in);
+	 int A=sc.nextInt();
+	 int B=sc.nextInt();
+	 int X=sc.nextInt();
+	 int Y=sc.nextInt();
+	 if(X>Y)
+	 System.out.println(A);
+	 else
+	 System.out.println(B);
+
+	}
+}
