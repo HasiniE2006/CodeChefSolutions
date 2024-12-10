051f442fd340be45192992d08b90e26584f484b5
Hasini E <hasini1530@gmail.com>
2024-12-10 23:18:21 +0530

    LUDO.java

---
+++ LUDO.java
@@ -0,0 +1,17 @@
+//START73
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args)
+	{
+	Scanner sc=new Scanner(System.in);
+	int t=sc.nextInt();
+	while(t!=0)
+	{
+	    int x=sc.nextInt();
+	    System.out.println((x==6)?"YES":"N0");
+	    t--;
+	}
+
+	}
+}
