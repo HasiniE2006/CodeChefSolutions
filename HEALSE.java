f95addf646f6074f2adc47430d4149bf9ceed49c
Hasini E <hasini1530@gmail.com>
2024-12-10 07:40:40 +0530

    HEALSE.java

---
+++ HEALSE.java
@@ -0,0 +1,16 @@
+//START122
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int H=sc.nextInt();
+		if(H<8)
+		 System.out.println("LESS");
+		else if(H==8)
+		System.out.println("PERFECT");
+		else
+		System.out.println("MORE");
+		}
+}
