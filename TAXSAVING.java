11fcd30e1547815fe0a49cb31d005e636e87d6e0
Hasini E <hasini1530@gmail.com>
2024-12-10 22:26:16 +0530

    TAXSAVING.java

---
+++ TAXSAVING.java
@@ -0,0 +1,16 @@
+//START75
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args)
+	{
+	 Scanner sc=new Scanner(System.in);
+	 int n=sc.nextInt();
+	 for(int i=0;i<n;i++)
+	 {
+	    int x=sc.nextInt();
+	    int y=sc.nextInt();
+	    System.out.println(x-y);
+	 }
+  }
+}
