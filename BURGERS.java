34bd3072cbeb4c05ad5f384750f6892649383ca3
Hasini E <hasini1530@gmail.com>
2024-12-11 22:26:26 +0530

    BURGERS.java

---
+++ BURGERS.java
@@ -0,0 +1,21 @@
+//START43
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args)
+	{
+	 Scanner sc=new Scanner(System.in);
+	 int t=sc.nextInt();
+	 while(t!=0)
+	 {
+	     int a=sc.nextInt();
+	     int b=sc.nextInt();
+	     if(a<b)
+	     System.out.println(a);
+	     else
+	     System.out.println(b);
+	     t--;
+	 }
+
+	}
+}
