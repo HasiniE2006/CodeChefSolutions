725b76e8b2b5fbe9ee66a284f9c43ab3cea30b22
Hasini E <hasini1530@gmail.com>
2024-12-11 22:20:45 +0530

    SWEETSHOP.java

---
+++ SWEETSHOP.java
@@ -0,0 +1,14 @@
+//START156
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		int n=sc.nextInt();
+		int z=x-(n*10);
+		int i=z/20;
+        System.out.println(i);
+	}
+}
