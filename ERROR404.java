17e42f97704ea85f4f8e67e1726456330e61fa6e
Hasini E <hasini1530@gmail.com>
2024-12-13 21:20:39 +0530

    ERROR404.java

---
+++ ERROR404.java
@@ -0,0 +1,12 @@
+//START111
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	 Scanner sc=new Scanner(System.in);
+	 int X=sc.nextInt();
+	 System.out.println((X==404)?"NOT FOUND":"FOUND");
+
+	}
+}
