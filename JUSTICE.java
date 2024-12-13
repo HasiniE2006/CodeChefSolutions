55def298f4dba1e19f39b544e51cf4b98a0431ff
Hasini E <hasini1530@gmail.com>
2024-12-13 20:58:34 +0530

    JUSTICE.java

---
+++ JUSTICE.java
@@ -0,0 +1,16 @@
+//START143
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	Scanner sc=new Scanner(System.in);
+	int x=sc.nextInt();
+	int y=sc.nextInt();
+	if(x>y || x==y)
+	System.out.println("YES");
+	else
+	System.out.println("NO");
+
+	}
+}
