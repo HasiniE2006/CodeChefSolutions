56cefde3c729386300f3d783d85302110c87d3e0
Hasini E <hasini1530@gmail.com>
2024-12-14 22:22:18 +0530

    IED.java

---
+++ IED.java
@@ -0,0 +1,17 @@
+//START118
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args)
+	{
+	 Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+	int b=sc.nextInt();
+	int c=sc.nextInt();
+	if(a>b)
+	System.out.println(a*c);
+	else
+	System.out.println(b*c);
+
+	}
+}
