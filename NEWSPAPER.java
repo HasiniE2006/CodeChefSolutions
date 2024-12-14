846b5669c476632dc05d4560da6f1f9867b4782d
Hasini E <hasini1530@gmail.com>
2024-12-14 22:51:37 +0530

    NEWSPAPER.java

---
+++ NEWSPAPER.java
@@ -0,0 +1,14 @@
+//START102
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	Scanner sc=new Scanner(System.in);
+	int x=sc.nextInt();
+	if((x>=7) && (x<=10))
+	System.out.println("YES");
+	else
+	System.out.println("NO");
+  }
+}
