3f4b35d3facf731af20fbb620b7e89efc32fcd06
Hasini E <hasini1530@gmail.com>
2025-01-01 21:52:14 +0530

    CRCK.java

---
+++ CRCK.java
@@ -0,0 +1,14 @@
+//START165
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args)
+	{
+	    Scanner sc=new Scanner(System.in);
+	 int n=sc.nextInt();
+	 int count=0;
+	 for(int i=n;i<25;i++)
+       count++;
+       System.out.println(count);
+	}
+}
