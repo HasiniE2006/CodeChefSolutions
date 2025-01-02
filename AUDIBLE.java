27d25055ff77af0bdea4ad5bda3c82d6f0dc44b7
Hasini E <hasini1530@gmail.com>
2025-01-02 21:47:06 +0530

    AUDIBLE.java

---
+++ AUDIBLE.java
@@ -0,0 +1,15 @@
+//START59
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args)
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	    if(n>=67 && n<=45000)
+	    System.out.println("Yes");
+	    else
+	    System.out.println("No");
+	    
+	}
+}
