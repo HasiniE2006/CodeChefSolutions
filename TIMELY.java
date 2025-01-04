ce1b8ebfe08854afd42b72dd1c0e459bee1d2288
Hasini E <hasini1530@gmail.com>
2025-01-04 21:42:21 +0530

    TIMELY.java

---
+++ TIMELY.java
@@ -0,0 +1,16 @@
+//START61
+class Codechef
+{
+	public static void main (String[] args)
+	{
+	    Scanner sc=new Scanner(System.in);
+	     int t=sc.nextInt();
+	     while(t!=0)
+	     {
+	         int a=sc.nextInt();
+	         System.out.println((a>=30)?"Yes":"No");
+	         t--;
+	     }
+
+	}
+}
