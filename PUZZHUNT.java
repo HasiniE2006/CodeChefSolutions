3e31ddf3bddbc8fab4184d1630a40b9601394373
Hasini E <hasini1530@gmail.com>
2025-01-04 21:50:08 +0530

    PUZZHUNT.java

---
+++ PUZZHUNT.java
@@ -0,0 +1,15 @@
+//START69
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int a=sc.nextInt();
+	    if(a>=6 && a<=8)
+	     System.out.println("yes");
+	    else
+	    System.out.println("No");
+
+	}
+}
