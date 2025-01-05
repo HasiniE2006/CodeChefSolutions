5b80449169f24541ded8aacc886a7718a3c2c531
Hasini E <hasini1530@gmail.com>
2025-01-05 20:30:59 +0530

    LUCKYSEVEN.java

---
+++ LUCKYSEVEN.java
@@ -0,0 +1,12 @@
+//SEP23
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args)
+	{
+	    Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+         String a=s.toLowerCase();
+        System.out.println(a.charAt(6));
+	}
+}
