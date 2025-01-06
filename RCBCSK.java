b375941d48b8d3edcf980c646492c921cbcbbe5b
Hasini E <hasini1530@gmail.com>
2025-01-06 22:12:38 +0530

    RCBCSK.java

---
+++ RCBCSK.java
@@ -0,0 +1,13 @@
+//START135
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	 Scanner sc=new Scanner(System.in);
+	 int x=sc.nextInt();
+	 int y=sc.nextInt();
+	 int z=x-y;
+	 System.out.println((z>=18)?"RCB":"CSK");
+     }
+}
