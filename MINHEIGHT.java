3aebc5b751b84a7d33b2f1be1146c74c8ac95088
Hasini E <hasini1530@gmail.com>
2025-01-07 23:06:04 +0530

    MINHEIGHT.java

---
+++ MINHEIGHT.java
@@ -0,0 +1,17 @@
+//START37
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t!=0)
+		{
+		int x=sc.nextInt();
+		int h=sc.nextInt();
+		System.out.println((x>=h)?"YES":"NO");
+		t--;
+		}
+	}
+}
