1863acea9cdd5b1eed6899a5037d65c403a3aae8
Hasini E <hasini1530@gmail.com>
2025-01-07 22:52:18 +0530

    RIP2000.java

---
+++ RIP2000.java
@@ -0,0 +1,14 @@
+//START97
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	 Scanner sc=new Scanner(System.in);
+	 int a=sc.nextInt();
+	 int b=a*2000;
+	 int c=b/500;
+	 System.out.println(c);
+
+	}
+}
