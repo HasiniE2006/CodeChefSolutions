25c8f44dfa899e329e62d48e32362ebeb0669687
Hasini E <hasini1530@gmail.com>
2025-01-08 20:58:51 +0530

    CCLEARN.java

---
+++ CCLEARN.java
@@ -0,0 +1,12 @@
+//START86
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		System.out.println(n*2);
+
+	}
+}
