9521786224b9e56257939b88bb8953c022623ccb
Hasini E <hasini1530@gmail.com>
2025-01-08 21:23:51 +0530

    COLDPLAYTICK.java

---
+++ COLDPLAYTICK.java
@@ -0,0 +1,12 @@
+//START154
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args)
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int x=sc.nextInt();
+	    System.out.println(5000+(x*5000));
+
+	}
+}
