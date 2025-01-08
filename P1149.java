1f30ab6d5ddae47fc294b572d5630df6c5f66b45
Hasini E <hasini1530@gmail.com>
2025-01-08 21:18:49 +0530

    P1149.java

---
+++ P1149.java
@@ -0,0 +1,18 @@
+//START149
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	    Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=sc.nextInt();
+        int a=x-y;
+        if(a>z)
+         System.out.println("NO");
+        else
+         System.out.println("YES");
+        
+	}
+}
