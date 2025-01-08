2383b6d229b535839d8ce47397bd494a2ef40000
Hasini E <hasini1530@gmail.com>
2025-01-08 21:12:56 +0530

    CANDIVIDE.java

---
+++ CANDIVIDE.java
@@ -0,0 +1,19 @@
+//START82
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args)
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int t=sc.nextInt();
+	    while(t!=0)
+	    {
+	        int a=sc.nextInt();
+	        if((a%3)==0)
+	         System.out.println("YES");
+	        else
+	         System.out.println("NO");
+	        t--;
+	    }
+ }
+}
