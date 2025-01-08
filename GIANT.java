95f3ebbfbbff4fa8ab16ab96e0f4ffe09d894e20
Hasini E <hasini1530@gmail.com>
2025-01-08 21:27:40 +0530

    GIANT.java

---
+++ GIANT.java
@@ -0,0 +1,11 @@
+//START130
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args)
+	{
+	 Scanner sc=new Scanner(System.in);
+	 int x=sc.nextInt();
+     System.out.println((x>=60)?"YES":"NO");
+	}
+}
