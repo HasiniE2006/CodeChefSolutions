14fe515df546c38c4b49fbbdfa3a859867e5076a
Hasini E <hasini1530@gmail.com>
2025-01-10 21:18:59 +0530

    HJJ.java

---
+++ HJJ.java
@@ -0,0 +1,11 @@
+//START168
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	 Scanner sc=new Scanner(System.in);
+	 int a=sc.nextInt();
+     System.out.println(a+20);
+	}
+}
