7a0fd3cb02a365a66db1e39721b926ba777fb954
Hasini E <hasini1530@gmail.com>
2025-01-10 21:26:19 +0530

    PRIZEPOOL.java

---
+++ PRIZEPOOL.java
@@ -0,0 +1,17 @@
+//MAY222
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	Scanner sc=new Scanner(System.in);
+	int t=sc.nextInt();
+	while(t!=0)
+	{
+	int x=sc.nextInt();
+	int y=sc.nextInt();
+	System.out.println((10*x)+(90*y));
+	t--;
+	}
+	}
+}
